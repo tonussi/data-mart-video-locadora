30a5c8e979e49536c70d4350ef91dcde5d244db1
caiosuarez <caiosasia@hotmail.com>
2020-10-20 22:31:12 -0300

    Add files via upload
    insert para tabela de filmes

---
+++ source/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO filmes (num_filme, nome_filme, diretor_filme, categoria_filme, estrela_filme, censura_filme, taxa_aluguel_filme, data_filme)
+VALUES (1, 'Quando em Roma', 'Mark Steven Johnson', 'comedia', 'Kristen Bell', 12, 6, '2010-01-29'),
+(2, 'Jogo de Amor em Las Vegas', 'Tom Vaughan', 'comedia', 'Ashton Kutcher', 12, 5, '2008-04-22'),
+(3, 'WALL-E', 'Andrew Stanton', 'animação', 'Ben Burtt', 0, 5, '2008-06-27' ),
+(4, ' Água para Elefantes' , 'Francis Lawrence' , 'Drama', 'Reese Witherspoon', 14, 5, '2011-04-29'),
+(5, 'Idas e Vindas do Amor', 'Garry Marshall', 'Romance', 'Julia Roberts', 12, 7, '2010-02-12'),
+(6, 'Crepúsculo' , 'Catherine Hardwicke', 'Romance', 'Kristen Stewart', 12, 5, '2018-12-19'),
+(7, 'A Origem' , 'Christopher Nolan', 'Ficção científica', 'Leonardo DiCaprio', 14, 8, '2010-08-06'),
+(8, 'O Poderoso Chefão', 'Francis Ford Coppola', 'Drama', 'Marlon Brando', 14, 7, '1972-03-24'),
+(9, 'Pulp Fiction', 'Quentin Tarantino', 'Crime', 'John Travolta', 18, 6, '1995-02-18'),
+(10, 'Coringa', 'Todd Phillips', 'Drama', 'Joaquin Phoenix', 16, 8, '2019-10-03')
