fe331e728b28a4cb268e76013c8f8bec20c378c2
caiosuarez <caiosasia@hotmail.com>
2020-10-22 00:41:44 -0300

    Update insert.sql

--- source/insert.sql
+++ source/insert.sql
@@ -54,8 +54,43 @@ VALUES (1, 1),
 (5, 9),
 (5, 10);
 
---INSERT INTO funcionarios (id_funcionario, id_locadora, id_supervisor, nome_funcionario, endereco_funcionario, cidade_funcionario, estado_funcionario, cep_funcionario, fone_funcionario, cpf_funcionario, ingresso_funcionario)
---VALUES (1, 1, 1, )
+INSERT INTO funcionarios (id_funcionario, id_locadora, id_supervisor, nome_funcionario, endereco_funcionario, cidade_funcionario, estado_funcionario, cep_funcionario, fone_funcionario, cpf_funcionario, ingresso_funcionario)
+VALUES (1, 1, 1, 'Rafael', 'Conselheiro Mafra, 333', 'florianopolis', 'SC', 88010101, 84841111, 11, '2019-10-22'),
+(2, 1, 1, 'Maria Eduarda', 'Abdon Batista, 31', 'florianopolis', 'SC', 88045108, 84842222, 12, '2019-10-22'),
+(3, 2, 3, 'Maria Clara', 'Abelardo Luz, 123', 'florianopolis', 'SC', 88075542, 84843333, 13, '2019-10-22'),
+(4, 2, 3, 'Nicolas', 'Travessa Abelardo Mateus Pinto, 11', 'florianopolis', 'SC', 88095135, 84844444, 14, '2019-10-22'),
+(5, 3, 5, 'Lara', 'Afonso Pena, 35', 'florianopolis', 'SC', 88070650, 84845555, 15, '2019-10-22'),
+(6, 3, 5, 'Guilherme', 'Agapito Veloso, 546', 'florianopolis', 'SC', 88075580, 84846666, 16, '2019-10-22'),
+(7, 4, 7, 'Gustavo', 'Servidão Apollo, 232', 'florianopolis', 'SC', 88045102, 84847777, 17, '2019-10-22'),
+(8, 4, 7, 'Lorena', 'Aprigio José da Silva, 765', 'florianopolis', 'SC', 88090380, 84848888, 18, '2019-10-22'),
+(9, 5, 9, 'Beatriz', 'Armando Calil Bulos, 278', 'florianopolis', 'SC', 88056618, 84849999, 19, '2019-10-22'),
+(10, 6, 10, 'Mariana', 'Armindo Martins Correia, 425', 'florianopolis', 'SC', 88056616, 84841010, 20, '2019-10-22');
+
+INSERT INTO copia_filmes (num_copia_filme, num_filme, id_locadora, valor_pago, condicao_geral, dta_compra)
+VALUES (1, 1, 1, 60, 10, '2020-10-22'),
+(2, 1, 1, 60, 10, '2020-10-22'),
+(3, 2, 2, 20, 10, '2020-10-22'),
+(4, 2, 2, 20, 10, '2020-10-22'),
+(5, 3, 3, 35, 10, '2020-10-22'),
+(6, 3, 3, 35, 10, '2020-10-22'),
+(7, 4, 4, 40, 10, '2020-10-22'),
+(8, 4, 4, 40, 10, '2020-10-22'),
+(9, 5, 5, 25, 10, '2020-10-22'),
+(10, 5, 5, 25, 10, '2020-10-22'),
+(11, 6, 6, 45, 10, '2020-10-22'),
+(12, 6, 6, 45, 10, '2020-10-22'),
+(13, 7, 1, 60, 10, '2020-10-22'),
+(14, 7, 1, 60, 10, '2020-10-22'),
+(15, 8, 2, 25, 10, '2020-10-22'),
+(16, 8, 2, 25, 10, '2020-10-22'),
+(17, 9, 3, 60, 10, '2020-10-22'),
+(18, 9, 3, 60, 10, '2020-10-22'),
+(19, 10, 4, 60, 10, '2020-10-22'),
+(20, 10, 4, 60, 10, '2020-10-22'),
+(21, 1, 5, 60, 10, '2020-10-22'),
+(22, 1, 5, 60, 10, '2020-10-22'),
+(23, 2, 6, 20, 10, '2020-10-22'),
+(24, 2, 6, 20, 10, '2020-10-22');
 
 
 
