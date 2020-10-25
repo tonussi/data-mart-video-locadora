69fca2c6481078a37774a0807846bb6f02323620
Lucas Tonussi <lucas@prevision.com.br>
2020-10-25 19:26:14 -0300

    Update fact_retorno_por_aluguel

--- datawarehouse_videoteca/models/facts/fact_retorno_por_aluguel.sql
+++ datawarehouse_videoteca/models/facts/fact_retorno_por_aluguel.sql
@@ -1,14 +1,20 @@
 select
-  source_raw_pagamentos.num_trans_pagamento,
-  source_raw_aluguel_filmes.num_copia_filme,
-  source_raw_pagamentos.data_pagamento,
-  sum(case when source_raw_aluguel_filmes.data_retorno_filme != null
-      then source_raw_pagamentos.quantia_pagamento + source_raw_aluguel_filmes.taxa_aluguel_filme
-      else source_raw_pagamentos.quantia_pagamento end) as quantia_pagamento_taxada
-  from {{ ref('raw_pagamentos') }} as source_raw_pagamentos
-  join {{ ref('raw_aluguel_filmes') }} as source_raw_aluguel_filmes
-  on source_raw_pagamentos.num_trans_pagamento = source_raw_aluguel_filmes.num_trans_pagamento
+  pagamentos.num_trans_pagamento,
+  aluguel_filmes.num_copia_filme,
+  pagamentos.data_pagamento,
+  (
+    case when aluguel_filmes.data_retorno_filme <> null
+    then pagamentos.quantia_pagamento + aluguel_filmes.taxa_aluguel_filme
+    else pagamentos.quantia_pagamento end
+  ) as quantia_pagamento_taxada
+  from {{ ref('dim_pagamentos') }} as pagamentos
+  join {{ ref('raw_aluguel_filmes') }} as aluguel_filmes
+  on pagamentos.num_trans_pagamento = aluguel_filmes.num_trans_pagamento
   group by (
-    source_raw_pagamentos.num_trans_pagamento,
-    source_raw_aluguel_filmes.num_copia_filme,
-    source_raw_pagamentos.data_pagamento)
+    pagamentos.num_trans_pagamento,
+    aluguel_filmes.num_copia_filme,
+    aluguel_filmes.data_retorno_filme,
+    pagamentos.quantia_pagamento,
+    aluguel_filmes.taxa_aluguel_filme,
+    pagamentos.data_pagamento
+  )
