8ddc0a8126d73d0b62712bd3647282177e9d3908
Lucas Tonussi <lucas@prevision.com.br>
2020-10-26 14:03:10 -0300

    Add max(data_pagamento)

--- datawarehouse_videoteca/models/dim/dim_pagamentos_somados.sql
+++ datawarehouse_videoteca/models/dim/dim_pagamentos_somados.sql
@@ -2,7 +2,8 @@ with source as (
   select
     pagamentos.num_trans_pagamento,
     aluguel_filmes.num_copia_filme,
-    pagamentos.quantia_pagamento
+    pagamentos.quantia_pagamento,
+    pagamentos.data_pagamento
     from {{ ref('dim_pagamentos') }} as pagamentos
     join {{ ref('raw_aluguel_filmes') }} as aluguel_filmes
     on pagamentos.num_trans_pagamento = aluguel_filmes.num_trans_pagamento
@@ -11,9 +12,10 @@ with source as (
 dim_pagamentos_somados as (
   select
     num_copia_filme,
+    max(data_pagamento),
     sum(quantia_pagamento)
   from source
-  group by (num_copia_filme, quantia_pagamento)
+  group by (num_copia_filme, data_pagamento, quantia_pagamento)
 )
 
 select * from dim_pagamentos_somados
