3b42a2b61290b5bdf13f1043d2eefcd0ca8d335c
Lucas Tonussi <lucas@prevision.com.br>
2020-10-26 13:34:30 -0300

    Add docker run that works fine

--- metabase.sh
+++ metabase.sh
@@ -1,9 +1,4 @@
 docker run -d -p 3005:3000 \
-  -e "MB_DB_TYPE=postgres" \
-  -e "MB_DB_DBNAME=datawarehouse_videoteca" \
-  -e "MB_DB_PORT=5433" \
-  -e "MB_DB_USER=postgres" \
-  -e "MB_DB_PASS=postgres" \
-  -e "MB_DB_HOST=localhost" \
+  -e "MB_DB_FILE=/metabase-data/metabase.db" \
   -v ~/metabase-data:/metabase-data \
   --name metabase metabase/metabase
