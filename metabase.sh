e446672f529f0ed855df42c3ef1616bb8a4cff5f
Lucas Tonussi <lucas@prevision.com.br>
2020-10-26 13:48:01 -0300

    Add docker run -d -p --network host

--- metabase.sh
+++ metabase.sh
@@ -1,4 +1,4 @@
-docker run -d -p 3005:3000 \
+docker run -d -p --network host \
   -e "MB_DB_FILE=/metabase-data/metabase.db" \
   -v ~/metabase-data:/metabase-data \
   --name metabase metabase/metabase
