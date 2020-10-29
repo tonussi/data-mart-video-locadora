b3f164083a9a081c7a7828b258ee56b472b2ea81
Lucas Tonussi <lucas@prevision.com.br>
2020-10-29 02:31:20 -0300

    Add comment

--- datawarehouse_videoteca/models/dim/dim_tempo.sql
+++ datawarehouse_videoteca/models/dim/dim_tempo.sql
@@ -7,7 +7,7 @@ dim_tempo as (
 select
   cast(data_retorno_filme as date)
   from source
-  where status_aluguel_filme = 'NA'
+  where status_aluguel_filme = 'NA' -- means NOT RENTED
 )
 
 select * from dim_tempo
