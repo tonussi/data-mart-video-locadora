25277ab20d12d32534af1edf661fd2858c0db78f
Lucas Tonussi <lucas@prevision.com.br>
2020-10-29 02:47:45 -0300

    Fix code

--- datawarehouse_videoteca/models/dim/dim_soma_taxa_por_copia_filme.sql
+++ datawarehouse_videoteca/models/dim/dim_soma_taxa_por_copia_filme.sql
@@ -3,14 +3,12 @@ with source as (
 ),
 
 intermediate_source as (
-
-select
-  num_copia_filme,
-  sum(taxa_aluguel_filme) as soma_taxa_aluguel_filme,
-  sum(sobretaxa_aluguel_filme) as soma_sobretaxa_aluguel_filme
+  select
+      num_copia_filme,
+      sum(taxa_aluguel_filme) as soma_taxa_aluguel_filme,
+      sum(sobretaxa_aluguel_filme) as soma_sobretaxa_aluguel_filme
   from source
   group by (num_copia_filme, taxa_aluguel_filme, sobretaxa_aluguel_filme)
-
 ),
 
 dim_soma_taxa_por_copia_filme as (
@@ -18,7 +16,6 @@ dim_soma_taxa_por_copia_filme as (
     num_copia_filme,
     soma_taxa_aluguel_filme + soma_sobretaxa_aluguel_filme as receita_somada_aluguel_filme
   from intermediate_source
-
 )
 
 select * from dim_soma_taxa_por_copia_filme
