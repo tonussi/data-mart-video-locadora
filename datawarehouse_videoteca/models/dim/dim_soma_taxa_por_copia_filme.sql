af02ee39a3f50f5b2deada81ac91f429457a4ed6
Lucas Tonussi <lucas@prevision.com.br>
2020-10-29 02:45:41 -0300

    Fix dim

--- datawarehouse_videoteca/models/dim/dim_soma_taxa_por_copia_filme.sql
+++ datawarehouse_videoteca/models/dim/dim_soma_taxa_por_copia_filme.sql
@@ -2,13 +2,23 @@ with source as (
   select * from {{ ref('raw_aluguel_filmes') }}
 ),
 
-dim_soma_taxa_por_copia_filme as (
+intermediate_source as (
 
 select
   num_copia_filme,
-  sum(taxa_aluguel_filme) as soma_taxa_aluguel_filme
+  sum(taxa_aluguel_filme) as soma_taxa_aluguel_filme,
+  sum(sobretaxa_aluguel_filme) as soma_sobretaxa_aluguel_filme
   from source
-  group by (num_copia_filme, taxa_aluguel_filme)
+  group by (num_copia_filme, taxa_aluguel_filme, sobretaxa_aluguel_filme)
+
+),
+
+dim_soma_taxa_por_copia_filme as (
+  select
+    num_copia_filme,
+    soma_taxa_aluguel_filme + soma_sobretaxa_aluguel_filme as receita_somada_aluguel_filme
+  from intermediate_source
+
 )
 
 select * from dim_soma_taxa_por_copia_filme
