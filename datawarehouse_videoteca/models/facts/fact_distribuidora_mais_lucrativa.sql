cd6e62c9e62347724dc32a05a218f8c46ed664f9
Lucas Tonussi <lucas@prevision.com.br>
2020-10-31 16:19:22 -0300

    Upgrade dims add fact

--- datawarehouse_videoteca/models/facts/fact_distribuidora_mais_lucrativa.sql
+++ datawarehouse_videoteca/models/facts/fact_distribuidora_mais_lucrativa.sql
@@ -5,7 +5,8 @@ with source as (
     dim_locadoras.id_distribuidor,
     dim_aluguel_filmes.num_copia_filme,
     dim_aluguel_filmes.taxa_aluguel_filme,
-    dim_aluguel_filmes.sobretaxa_aluguel_filme
+    dim_aluguel_filmes.sobretaxa_aluguel_filme,
+    (select count(*) from dim_aluguel_filmes) as total_locacoes_todas_locadoras
   from {{ ref('dim_locadoras') }} as dim_locadoras
   join {{ ref('dim_copia_filmes_e_suas_locadoras') }} as dim_copia_filmes_e_suas_locadoras
   on dim_locadoras.id_locadora = dim_copia_filmes_e_suas_locadoras.id_locadora
@@ -19,12 +20,15 @@ fact_distribuidora_mais_lucrativa as (
     nome_locadora,
     id_distribuidor,
     num_copia_filme,
+    total_locacoes_todas_locadoras,
     (taxa_aluguel_filme + sobretaxa_aluguel_filme) as lucro_da_locadora
   from source
 )
 
 select
   id_locadora,
-  sum(lucro_da_locadora)
+  sum(lucro_da_locadora) as soma_lucro_por_locadora,
+  total_locacoes_todas_locadoras,
+  trunc(sum(lucro_da_locadora) / total_locacoes_todas_locadoras, 3) as razao_soma_lucro_por_total
 from fact_distribuidora_mais_lucrativa
-group by 1
+group by (id_locadora, total_locacoes_todas_locadoras)
