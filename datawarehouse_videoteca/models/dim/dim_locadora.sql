ba59cc4efddd0151b22dc7f50a49fc0bc2b57c1a
Lucas Tonussi <lucas@prevision.com.br>
2020-10-31 01:18:33 -0300

    Fix dim locadoras

--- datawarehouse_videoteca/models/dim/dim_locadora.sql
+++ datawarehouse_videoteca/models/dim/dim_locadora.sql
@@ -1,13 +1,19 @@
 with source as (
-  select * from {{ ref('raw_locadoras') }}
+  select
+    raw_locadoras.id_locadora,
+    raw_locadora_distribuidores.id_distribuidor,
+    raw_locadoras.nome_locadora
+  from {{ ref('raw_locadoras') }} as raw_locadoras
+  join {{ ref('raw_locadora_distribuidores') }} as raw_locadora_distribuidores
+  on raw_locadoras.id_locadora = raw_locadora_distribuidores.id_locadora
 ),
 
 dim_locadora as (
   select
     id_locadora,
-    fone_locadora
+    id_distribuidor,
+    nome_locadora
   from source
 )
 
 select * from dim_locadora
-
